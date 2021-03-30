5b2b907a495ca5b7fb53ea18595616e25bcfd03f
Jairo Gimenes <jagimenes@gmail.com>
2021-03-30 08:40:36 -0300

    Desafio finalizado

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,17 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return this.repository.findOneOrFail(user_id, {relations: ["games"]});
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM USERS ORDER BY FIRST_NAME");
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM USERS WHERE UPPER(FIRST_NAME) = UPPER($1) AND UPPER(LAST_NAME) = UPPER($2)', [first_name, last_name]);
   }
 }
